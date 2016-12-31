34f6a0c7e6f91214727a47f3441f3466e9221fcb
RoboMWM <robomwm@users.noreply.github.com>
2016-12-30 18:00:18 -0800

    Use ActionBar, use ProjectileCollideEvent

--- src/main/java/to/us/techfort/DisablePvP/DisablePvP.java
+++ src/main/java/to/us/techfort/DisablePvP/DisablePvP.java
@@ -1,8 +1,10 @@
 package to.us.techfort.DisablePvP;
 
+import com.destroystokyo.paper.event.entity.ProjectileCollideEvent;
 import me.ryanhamshire.GriefPrevention.Claim;
 import me.ryanhamshire.GriefPrevention.DataStore;
 import me.ryanhamshire.GriefPrevention.GriefPrevention;
+import me.ryanhamshire.GriefPrevention.PlayerData;
 import me.ryanhamshire.GriefPrevention.events.PreventPvPEvent;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
@@ -23,6 +25,7 @@ import org.bukkit.event.entity.EntityCombustByEntityEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.PotionSplashEvent;
+import org.bukkit.event.entity.ProjectileHitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
@@ -36,8 +39,7 @@ import java.util.Set;
 /**
  * Created by Robo on 9/1/2016.
  */
-public class DisablePvP extends JavaPlugin implements Listener
-{
+public class DisablePvP extends JavaPlugin implements Listener {
     String disabledMessage = ChatColor.RED + "You turned /pvp off";
     String enabledMessage = ChatColor.GREEN + "You turned /pvp on";
     String attackerDisabled = ChatColor.RED + "You disabled /pvp";
@@ -49,15 +51,13 @@ public class DisablePvP extends JavaPlugin implements Listener
     FileConfiguration config = getConfig();
     DataStore ds;
     Set<PotionEffectType> positiveEffects;
-    boolean isPvPDisabledPlayer(Player player)
-    {
+
+    boolean isPvPDisabledPlayer(Player player) {
         return config.getStringList("playersDisabled").contains(player.getUniqueId().toString());
     }
 
-    void addPvPDisabledPlayer(Player player)
-    {
-        if (!isPvPDisabledPlayer(player))
-        {
+    void addPvPDisabledPlayer(Player player) {
+        if (!isPvPDisabledPlayer(player)) {
             List<String> newList = config.getStringList("playersDisabled");
             newList.add(player.getUniqueId().toString());
             config.set("playersDisabled", newList);
@@ -65,23 +65,19 @@ public class DisablePvP extends JavaPlugin implements Listener
         saveConfig();
     }
 
-    void removePvPDisabledPlayer(Player player)
-    {
+    void removePvPDisabledPlayer(Player player) {
         List<String> newList = config.getStringList("playersDisabled");
         newList.remove(player.getUniqueId().toString());
         config.set("playersDisabled", newList);
         saveConfig();
     }
 
-    boolean isPvPEnabledClaim(Claim claim)
-    {
+    boolean isPvPEnabledClaim(Claim claim) {
         return config.getStringList("claimsEnabled").contains(claim.getID().toString());
     }
 
-    void addPvPEnabledClaim(Claim claim)
-    {
-        if (!isPvPEnabledClaim(claim))
-        {
+    void addPvPEnabledClaim(Claim claim) {
+        if (!isPvPEnabledClaim(claim)) {
             List<String> newList = config.getStringList("claimsEnabled");
             newList.add(claim.getID().toString());
             config.set("claimsEnabled", newList);
@@ -89,21 +85,19 @@ public class DisablePvP extends JavaPlugin implements Listener
         saveConfig();
     }
 
-    void removePvPEnabledClaim(Claim claim)
-    {
+    void removePvPEnabledClaim(Claim claim) {
         List<String> newList = config.getStringList("claimsEnabled");
         newList.remove(claim.getID().toString());
         config.set("claimsEnabled", newList);
         saveConfig();
     }
 
-    public void onEnable()
-    {
+    public void onEnable() {
         config.addDefault("playersDisabled", new ArrayList<String>());
         config.addDefault("claimsEnabled", new ArrayList<String>());
         config.options().copyDefaults(true);
         saveConfig();
-        GriefPrevention gp = (GriefPrevention)getServer().getPluginManager().getPlugin("GriefPrevention");
+        GriefPrevention gp = (GriefPrevention) getServer().getPluginManager().getPlugin("GriefPrevention");
         ds = gp.dataStore;
         getServer().getPluginManager().registerEvents(this, this);
         positiveEffects = new HashSet<>(Arrays.asList
@@ -127,56 +121,43 @@ public class DisablePvP extends JavaPlugin implements Listener
     }
 
     @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
-    {
-        if (!(sender instanceof Player))
-        {
-            if (cmd.getName().equalsIgnoreCase("pvp"))
-            {
-                if (args.length < 2)
-                {
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            if (cmd.getName().equalsIgnoreCase("pvp")) {
+                if (args.length < 2) {
                     sender.sendMessage("/pvp <on|off|check> <player>");
                     return true;
                 }
                 Player player = Bukkit.getPlayerExact(args[1]);
-                if (player == null)
-                {
+                if (player == null) {
                     sender.sendMessage(args[1] + " ain't a player.");
                     return true;
                 }
-                if (args[0].equalsIgnoreCase("on"))
-                {
+                if (args[0].equalsIgnoreCase("on")) {
                     enablePvP(player);
                     sender.sendMessage("Enabled PvP for " + player.getName());
-                }
-                else if (args[0].equalsIgnoreCase("off"))
-                {
+                } else if (args[0].equalsIgnoreCase("off")) {
                     disablePvP(player);
                     sender.sendMessage("Disabled PvP for " + player.getName());
-                }
-                else if (args[0].equalsIgnoreCase("check"))
-                {
+                } else if (args[0].equalsIgnoreCase("check")) {
                     if (isPvPDisabledPlayer(player))
                         sender.sendMessage(player.getName() + " disabled PvP");
                     else
                         sender.sendMessage(player.getName() + " did not disable PvP");
                     return true;
-                }
-                else
+                } else
                     sender.sendMessage("/pvp <on|off|check> <player>");
                 return true;
             }
             return false;
         }
 
-        Player player = (Player)sender;
+        Player player = (Player) sender;
 
-        if (cmd.getName().equalsIgnoreCase("pvp"))
-        {
+        if (cmd.getName().equalsIgnoreCase("pvp")) {
             if (args.length > 0) //if option is explicitly stated
             {
-                switch (args[0].toLowerCase())
-                {
+                switch (args[0].toLowerCase()) {
                     case "disable":
                     case "off":
                     case "false":
@@ -198,32 +179,25 @@ public class DisablePvP extends JavaPlugin implements Listener
             return true;
         }
 
-        if (cmd.getName().equalsIgnoreCase("claimpvp"))
-        {
+        if (cmd.getName().equalsIgnoreCase("claimpvp")) {
             Claim claim = ds.getClaimAt(player.getLocation(), true, null);
-            if (claim == null)
-            {
+            if (claim == null) {
                 player.sendMessage(noClaim);
                 return true;
             }
             String notAllowed = claim.allowGrantPermission(player);
             if (notAllowed == null) //e.g. allowed to do this
             {
-                if (isPvPEnabledClaim(claim))
-                {
+                if (isPvPEnabledClaim(claim)) {
                     removePvPEnabledClaim(claim);
                     player.sendMessage(claimPvPDisabled);
                     return true;
-                }
-                else
-                {
+                } else {
                     addPvPEnabledClaim(claim);
                     player.sendMessage(claimPvPEnabled);
                     return true;
                 }
-            }
-            else
-            {
+            } else {
                 player.sendMessage(ChatColor.RED + notAllowed);
                 return true;
             }
@@ -231,27 +205,23 @@ public class DisablePvP extends JavaPlugin implements Listener
         return false;
     }
 
-    void disablePvP(Player player)
-    {
+    void disablePvP(Player player) {
         addPvPDisabledPlayer(player);
         player.sendMessage(disabledMessage);
     }
 
-    void enablePvP(Player player)
-    {
+    void enablePvP(Player player) {
         removePvPDisabledPlayer(player);
         player.sendMessage(enabledMessage);
     }
 
     @EventHandler(ignoreCancelled = true, priority = EventPriority.LOWEST)
-    void onPlayerDamage(EntityDamageByEntityEvent event)
-    {
+    void onPlayerDamage(EntityDamageByEntityEvent event) {
         handleEntityDamageEventCuzThxSpigot(event);
     }
 
     @EventHandler(ignoreCancelled = true, priority = EventPriority.HIGHEST)
-    void onPotionSplash(PotionSplashEvent event)
-    {
+    void onPotionSplash(PotionSplashEvent event) {
         ThrownPotion potion = event.getPotion();
         //Check if a player threw the potion
         if (!(potion.getShooter() instanceof Player))
@@ -259,10 +229,8 @@ public class DisablePvP extends JavaPlugin implements Listener
 
         //Check if this potion has harmful effects
         boolean isHarmful = false;
-        for (PotionEffect effect : potion.getEffects())
-        {
-            if (!positiveEffects.contains(effect))
-            {
+        for (PotionEffect effect : potion.getEffects()) {
+            if (!positiveEffects.contains(effect)) {
                 isHarmful = true;
                 break;
             }
@@ -271,30 +239,26 @@ public class DisablePvP extends JavaPlugin implements Listener
             return;
 
         //Mark whether the shooter disabled PvP or not
-        boolean shooterDisabledPvP = isPvPDisabledPlayer((Player)potion.getShooter());
+        boolean shooterDisabledPvP = isPvPDisabledPlayer((Player) potion.getShooter());
 
         //Check if affected entities are players with PvP disabled (except the thrower)
-        for (LivingEntity entity : event.getAffectedEntities())
-        {
-            if (entity instanceof Player)
-            {
-                if ((shooterDisabledPvP || isPvPDisabledPlayer((Player)entity)) && entity != potion.getShooter())
+        for (LivingEntity entity : event.getAffectedEntities()) {
+            if (entity instanceof Player) {
+                if ((shooterDisabledPvP || isPvPDisabledPlayer((Player) entity)) && entity != potion.getShooter())
                     event.setIntensity(entity, 0);
             }
         }
     }
 
     @EventHandler(ignoreCancelled = true, priority = EventPriority.LOWEST)
-    void onPlayerIgniteWithArrow(EntityCombustByEntityEvent event)
-    {
-        EntityDamageByEntityEvent eventWrapper = new EntityDamageByEntityEvent(event.getCombuster(), event.getEntity(), EntityDamageEvent.DamageCause.FIRE_TICK, (double)event.getDuration());
+    void onPlayerIgniteWithArrow(EntityCombustByEntityEvent event) {
+        EntityDamageByEntityEvent eventWrapper = new EntityDamageByEntityEvent(event.getCombuster(), event.getEntity(), EntityDamageEvent.DamageCause.FIRE_TICK, (double) event.getDuration());
         handleEntityDamageEventCuzThxSpigot(eventWrapper);
         event.setCancelled(eventWrapper.isCancelled());
     }
 
     @EventHandler
-    void onGPPreventingPvPInAClaim(PreventPvPEvent event)
-    {
+    void onGPPreventingPvPInAClaim(PreventPvPEvent event) {
         Claim claim = event.getClaim();
         if (claim == null)
             return;
@@ -304,8 +268,7 @@ public class DisablePvP extends JavaPlugin implements Listener
     }
 
     //Credit to BigScary for some of this thanks to the spigot-madness of changing this event >_>
-    void handleEntityDamageEventCuzThxSpigot(EntityDamageByEntityEvent event)
-    {
+    void handleEntityDamageEventCuzThxSpigot(EntityDamageByEntityEvent event) {
         Entity damager = event.getDamager();
         //Check if victim is player and if attacker is a player or victim
         if (event.getEntityType() != EntityType.PLAYER || (damager.getType() != EntityType.PLAYER && damager.getType() != EntityType.ARROW && damager.getType() != EntityType.TIPPED_ARROW && damager.getType() != EntityType.SPECTRAL_ARROW))
@@ -313,36 +276,126 @@ public class DisablePvP extends JavaPlugin implements Listener
 
         //Get the attacker
         Player attacker = null;
-        switch (damager.getType())
-        {
+        switch (damager.getType()) {
             case ARROW:
             case TIPPED_ARROW:
-			case SPECTRAL_ARROW:
-                Projectile arrow = (Projectile)damager;
+            case SPECTRAL_ARROW:
+                Projectile arrow = (Projectile) damager;
                 if (!(arrow.getShooter() instanceof Player))
                     return; //Dispenser
-                attacker = (Player)arrow.getShooter();
+                attacker = (Player) arrow.getShooter();
                 break;
             case PLAYER:
-                attacker = (Player)damager;
+                attacker = (Player) damager;
         }
 
         //Check if attacker disabled PvP
-        if (isPvPDisabledPlayer(attacker))
-        {
+        if (isPvPDisabledPlayer(attacker)) {
             event.setCancelled(true);
-            attacker.sendMessage(attackerDisabled);
+            attacker.sendActionBar(attackerDisabled);
             return;
         }
 
         //Attacker has PvP enabled; check if victim disabled PvP
-        Player victim = (Player)event.getEntity();
+        Player victim = (Player) event.getEntity();
 
-        if (isPvPDisabledPlayer(victim))
-        {
+        if (isPvPDisabledPlayer(victim)) {
             event.setCancelled(true);
-            attacker.sendMessage(victimDisabled);
+            attacker.sendActionBar(victimDisabled);
             return; //redundant, but in case I want to add to this method in the future...
+        }
+    }
+
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.HIGHEST)
+    void onProjectileCollide(ProjectileCollideEvent event) {
+        //Is this copy-pasted code? Is this bad practice?
+        //You sure betcha it is!!!!!1111!!
+
+        //Check if victim is a player
+        if (event.getCollidedWith().getType() != EntityType.PLAYER)
+            return;
+
+        //Check if attacker is a player
+        if (!(event.getEntity().getShooter() instanceof Player))
+            return;
+
+        Entity damager = event.getEntity();
+
+        //Get the attacker
+        Player attacker;
+        switch (damager.getType()) {
+            case ARROW:
+            case TIPPED_ARROW:
+            case SPECTRAL_ARROW:
+                attacker = (Player)event.getEntity().getShooter();
+                break;
+            default: //Please
+                return;
+        }
+
+        //Check if attacker disabled PvP
+        if (isPvPDisabledPlayer(attacker)) {
+            event.setCancelled(true);
+            return;
+        }
+
+        //Attacker has PvP enabled; check if victim disabled PvP
+        Player victim = (Player) event.getEntity();
+
+        if (isPvPDisabledPlayer(victim)) {
+            event.setCancelled(true);
+            return; //redundant, but in case I want to add to this method in the future...
+        }
+    }
+
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.HIGH)
+    void onProjectileCollideInClaim(ProjectileCollideEvent event)
+    {
+        if (event.getCollidedWith().getType() != EntityType.PLAYER)
+            return;
+
+        if (!(event.getEntity().getShooter() instanceof Player))
+            return;
+
+        //determine which player is attacking, if any
+        Player attacker = (Player)event.getEntity().getShooter();
+
+        PlayerData attackerData = ds.getPlayerData(attacker.getUniqueId());
+
+        Claim attackerClaim = this.ds.getClaimAt(attacker.getLocation(), false, attackerData.lastClaim);
+        if(!attackerData.ignoreClaims)
+        {
+            if( attackerClaim != null && //ignore claims mode allows for pvp inside land claims
+                    !attackerData.inPvpCombat() &&
+                    GriefPrevention.instance.claimIsPvPSafeZone(attackerClaim))
+            {
+                attackerData.lastClaim = attackerClaim;
+                PreventPvPEvent pvpEvent = new PreventPvPEvent(attackerClaim);
+                Bukkit.getPluginManager().callEvent(pvpEvent);
+                if(!pvpEvent.isCancelled())
+                {
+                    event.setCancelled(true);
+                    return;
+                }
+            }
+
+            Player defender = (Player)event.getCollidedWith();
+            PlayerData defenderData = ds.getPlayerData(defender.getUniqueId());
+
+            Claim defenderClaim = this.ds.getClaimAt(defender.getLocation(), false, defenderData.lastClaim);
+            if( defenderClaim != null &&
+                    !defenderData.inPvpCombat() &&
+                    GriefPrevention.instance.claimIsPvPSafeZone(defenderClaim))
+            {
+                defenderData.lastClaim = defenderClaim;
+                PreventPvPEvent pvpEvent = new PreventPvPEvent(defenderClaim);
+                Bukkit.getPluginManager().callEvent(pvpEvent);
+                if(!pvpEvent.isCancelled())
+                {
+                    event.setCancelled(true);
+                    return;
+                }
+            }
         }
     }
 }
